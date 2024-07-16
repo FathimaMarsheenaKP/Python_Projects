8acfa2f86d10248de9af6e9ac6fe61ebbe9f05b9
fathimamarsheenakp <fathimamarsheena@gmail.com>
2024-07-16 12:45:27 +0530

    Add DataEntryToExcel

--- DataEntryToExcel.py
+++ DataEntryToExcel.py
@@ -1,4 +1,4 @@
-from openpyxl import workbook, load_workbook
+from openpyxl import Workbook, load_workbook
 import tkinter as tk
 from tkinter import Label, Entry, Button
 ################################
@@ -9,7 +9,7 @@ class DataEntryForm(tk.Tk):
 
 
         self.title("Data Entry Form")
-        self.geometry("500x400")
+        self.geometry("500x200")
 
         self.item_label = Label(self, text = 'Item')
         self.item_entry = Entry(self)
@@ -18,7 +18,45 @@ class DataEntryForm(tk.Tk):
         self.quantity_entry = Entry(self)
 
         self.price_label = Label(self, text = 'Price')
-        self.price = Entry(self)
+        self.price_entry = Entry(self)
+
+        self.add_btn = Button(self, text = 'Add to Excel', command = self.add_data)
+
+        self.item_label.grid(row = 0, column = 0, padx=10, pady=10)
+        self.item_entry.grid(row = 0, column = 1, padx=10, pady=10)
+
+        self.quantity_label.grid(row = 1, column = 0, padx=10, pady=10)
+        self.quantity_entry.grid(row = 1, column = 1, padx=10, pady=10)
+
+        self.price_label.grid(row = 2, column = 0, padx=10, pady=10)
+        self.price_entry.grid(row = 2, column = 1, padx=10, pady=10)
+
+        self.add_btn.grid(row = 3, column = 2, padx=10, pady=10)
+
+    def add_data(self):
+        item = self.item_entry.get()
+        quantity = self.quantity_entry.get()
+        price = self.price_entry.get()
+
+        if item and quantity and price:
+            self.write_to_excel(item, quantity, price)
+
+        self.item_entry.delete(0,'end')
+        self.quantity_entry.delete(0,'end')
+        self.price_entry.delete(0,'end')
+
+    def write_to_excel(self, item, quantity, price):
+        try:
+            workbook = load_workbook('item.xlsx')
+        except FileNotFoundError:
+            workbook = Workbook()
+
+        sheet = workbook.active
+
+        row = [item, quantity, price]
+        sheet.append(row)
+
+        workbook.save('item.xlsx')
 
 
 
